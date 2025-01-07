1b38d445ba6534bd08ee4e7eaa767660863b99e9
ziynatilloh-html <ziynatillokht@gmail.com>
2025-01-07 19:45:01 +0900

    fix: MUI-customized integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { RippleBadge } from "./MaterialTheme/styled";
 
 function App() {
   return (
@@ -11,7 +12,12 @@ function App() {
             Create React App on TypeScript with REDUX
           </Typography>
         </Box>
-        <Button variant="contained">Contained</Button>
+        <Box>
+          <RippleBadge badgeContent={4}>
+            {" "}
+            <Button variant="contained">Check messages</Button>
+          </RippleBadge>
+        </Box>
       </Stack>
     </Container>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { Provider } from "react-redux";
-// import { store } from "./app/store";
-// import App from "./app/App";
-// import reportWebVitals from "./reportWebVitals";
-// import CssBaseline from "@mui/material/CssBaseline";
-// import { ThemeProvider } from "@mui/material/styles";
-// import theme from "./app/theme";
-// import "./css/index.css";
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <ThemeProvider theme={theme}><ThemeProvider>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
@@ -27,8 +6,8 @@ import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "./app/theme";
 import "./css/index.css";
+import theme from "./app/MaterialTheme";
 
 ReactDOM.render(
   <React.StrictMode>
