987295ec51fbbb20eac7a3b3cd3e39de49bd8383
ziynatilloh-html <ziynatillokht@gmail.com>
2025-01-10 19:32:27 +0900

    feat: develop burak screen-based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,42 +4,48 @@ import "../css/app.css";
 import { Route, Switch } from "react-router";
 import { Link } from "react-router-dom";
 import { Container } from "@mui/material";
-import { About } from "./screens/About";
-import { Users } from "./screens/Users";
+import { HomePage } from "./screens/homePage";
+import { ProductsPage } from "./screens/productsPage";
+import { OrdersPage } from "./screens/ordersPage";
+import { UserPage } from "./screens/userPage";
 function App() {
   return (
     <div>
       <nav>
         <ul>
+          {" "}
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">HomePage</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/products">ProductsPage</Link>
           </li>
           <li>
-            <Link to="/users">Users</Link>
+            <Link to="/orders">OrdersPage</Link>
+          </li>
+          <li>
+            <Link to="/member-page">UserPage</Link>
           </li>
         </ul>
       </nav>
 
-      {/* A <Switch> looks through its children <Route>s and renders the first one that matches the current URL. */}
+      {/* A <Switch> looks through its children <Route>s and renders the first onethat matches the current URL. */}
       <Switch>
-        <Route path="/about">
-          <About />
+        <Route path="/products">
+          <ProductsPage />
         </Route>
-        <Route path="/users">
-          <Users />
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
         </Route>
         <Route path="/">
-          <Home />
+          <HomePage />
         </Route>
       </Switch>
     </div>
   );
-}
-function Home() {
-  return <Container> Home</Container>;
 }
 
 export default App;
--- src/app/screens/Users.tsx
+++
@@ -1,5 +0,0 @@
-import Container from "@mui/material/Container";
-
-export function Users() {
-  return <Container> Users</Container>;
-}
