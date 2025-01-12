dd64a324097ebd779664fa24c2e070e29563c4b1
ziynatilloh-html <ziynatillokht@gmail.com>
2025-01-12 20:49:12 +0900

    fix: modify homeNavbar header component

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -58,7 +58,25 @@ export function HomeNavbar() {
             )}
           </Stack>
         </Stack>
-        <Stack>DETAIL</Stack>
+        <Stack className={"header-frame"}>
+          <Stack className={"detail"}>
+            <Box className="head-main-txt">World's Most Delicious Cuisine</Box>
+            <Box className="wel-txt">The Choice, not just a choice</Box>
+            <Box className="service-txt">24 hours service</Box>
+            <Box className="signup">
+              {!authMember ? (
+                <Button variant={"contained"} className="signup-button">
+                  SIGN UP
+                </Button>
+              ) : null}
+            </Box>
+          </Stack>
+          <Stack>
+            <Box className={"logo-frame"}>
+              <div className={"logo-img"}></div>
+            </Box>
+          </Stack>
+        </Stack>
       </Container>
     </div>
   );
