789211375ac72bc491c84647ca08d2beb33fe8f7
ziynatilloh-html <ziynatillokht@gmail.com>
2025-01-17 04:22:04 +0900

    feat: develop productPage nested routing system

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,5 +1,22 @@
+import React from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
 import { Container } from "@mui/material";
+import ChosenProduct from "./ChosenProduct";
+import Products from "./Products";
 
 export default function ProductsPage() {
-  return <Container> ProductsPage</Container>;
+  const products = useRouteMatch();
+  console.log(products);
+  return (
+    <div className="products-page">
+      <Switch>
+        <Route path={`${products.path}/:productId`}>
+          <ChosenProduct />
+        </Route>
+        <Route path={`${products.path}`}>
+          <Products />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
