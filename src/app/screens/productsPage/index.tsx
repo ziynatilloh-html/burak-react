b3c069a129a7684ac4a021330db7067f10e86217
ziynatilloh-html <ziynatillokht@gmail.com>
2025-01-17 21:30:25 +0900

    feat: chosenproduct sectional component

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -3,6 +3,7 @@ import { Route, Switch, useRouteMatch } from "react-router-dom";
 import { Container } from "@mui/material";
 import ChosenProduct from "./ChosenProduct";
 import Products from "./Products";
+import "../../../css/products.css";
 
 export default function ProductsPage() {
   const products = useRouteMatch();
