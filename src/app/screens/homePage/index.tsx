4018b052c9e861fe722364d0d461be630ef86013
ziynatilloh-html <ziynatillokht@gmail.com>
2025-02-07 18:08:19 +0900

    feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -6,15 +6,33 @@ import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css";
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularDishes } from "./slice";
+import { retrievePopularDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
+
+// Redux Slice & Selector
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+});
+
+const popularDishesRetriever = createSelector(
+  retrievePopularDishes,
+  (popularDishes) => popularDishes
+);
 
 export default function HomePage() {
-  //select=> Store  :data
-  useEffect(() => {}, []);
-  //Backend  server data request = Data
-  // slice:Data = Store
+  const { setPopularDishes } = actionDispatch(useDispatch());
+  const popularDishes = useSelector(popularDishesRetriever);
+
+  useEffect(() => {
+    console.log("popularDishes:", popularDishes);
+  }, []);
 
   return (
-    <div className={"homepage"}>
+    <div className="homepage">
       <Statistics />
       <PopularDishes />
       <NewDishes />
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,19 +1,14 @@
-import { HomePageState } from "./../lib/types/screen";
-import {
-  configureStore,
-  ThunkAction,
-  Action,
-  AnyAction,
-  getDefaultMiddleware,
-} from "@reduxjs/toolkit";
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import HomePageReducer from "./screens/homePage/slice";
 import reduxLogger from "redux-logger";
 
 export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     //@ts-ignore
     getDefaultMiddleware().concat(reduxLogger),
-
-  reducer: { homePage: HomePageReducer },
+  reducer: {
+    homePage: HomePageReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
@@ -24,6 +19,3 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   unknown,
   Action<string>
 >;
-function HomePageReducer(state: unknown, action: AnyAction): unknown {
-  throw new Error("Function not implemented.");
-}
